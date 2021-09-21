292de476ecfbd1f8813113271e72f53051b3175c
Eugene Kang <kangeuge90@gmail.com>
2021-09-21 15:56:28 -0400

    fixed spelling error

--- script.js
+++ script.js
@@ -143,7 +143,7 @@ function resetBoard() { // resets stored-card clicks and unlocks board
 
 const resetButton = document.querySelector(".reset");
 resetButton.addEventListener('click', (event) => {
-    cardmatched = 0;
+    cardMatched = 0;
     started = false;
     paused = false;
     victory = false;
